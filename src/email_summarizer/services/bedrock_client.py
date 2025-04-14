d721dbbe7aed38122f8311a90cb52a81e89e87f9
David Harmon <dharmon202@gmail.com>
2025-04-13 23:19:10 -0400

    Pull bedrock client logic into its own file

---
+++ src/email_summarizer/services/bedrock_client.py
@@ -0,0 +1,65 @@
+from typing import Any
+
+import os
+import boto3
+import json
+
+
+class BedrockClient():
+  boto3_client: Any
+  deepseek_inference_profile: str
+
+  def __init__(
+      self, 
+      service_name: str, 
+      region: str, 
+      aws_access_key: str, 
+      aws_secret_access_key: str,
+      deepseek_inference_profile: str
+  ):
+    self.boto3_client = boto3.client(
+      service_name=service_name,
+      region_name=region,
+      aws_access_key_id=aws_access_key,
+      aws_secret_access_key=aws_secret_access_key
+    )
+    self.deepseek_inference_profile = deepseek_inference_profile
+
+  def invoke_deepseek(self, prompt: str, temperature: float = 0.7, max_tokens: int = 500):
+    # Request body
+    body = json.dumps({
+        "prompt": prompt,
+        "temperature": temperature,
+        "max_tokens": max_tokens
+    })
+    # Send request
+    response = self.boto3_client.invoke_model(
+      modelId=self.deepseek_inference_profile,
+      body=body
+    )
+    # Read response
+    return json.loads(response['body'].read())
+    
+
+
+class BedrockClientFactory:
+  # class variables
+  service_name = 'bedrock-runtime'
+  region = 'us-east-1'
+
+  # instance variables
+  bedrock_client: BedrockClient | None
+
+  def __init__(self):
+    self.bedrock_client = None
+
+  def get_client(self):
+    if self.bedrock_client is None:
+      self.bedrock_client = BedrockClient(
+        service_name=BedrockClientFactory.service_name,
+        region=BedrockClientFactory.region,
+        aws_access_key=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        deepseek_inference_profile=os.environ.get("DEEPSEEK_INFERENCE_PROFILE")
+      )
+    return self.bedrock_client
