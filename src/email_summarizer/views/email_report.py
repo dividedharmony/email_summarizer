9cf61902c6fc49706de5d043683504eb29939622
David Harmon <dharmon202@gmail.com>
2025-04-20 23:28:54 -0400

    Remove vestigial files

--- src/email_summarizer/views/email_report.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-from email_summarizer.services.gmail import Email
-
-
-class EmailReport(BaseModel):
-    summaries: list[str]
-    emails: list[Email]
-    today: str
