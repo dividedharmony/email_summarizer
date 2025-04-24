9abe8bae8c933265fafe3ba0e80fe76c18e5f33b
David Harmon <dharmon202@gmail.com>
2025-04-24 14:15:13 -0400

    Add SCHOOL, REVIEW, and expanded SOCIAL email tags

--- src/email_summarizer/prompts/summary_prompt.py
+++ src/email_summarizer/prompts/summary_prompt.py
@@ -11,9 +11,11 @@ the email was sent. All emails fall under one of the following categories:
 |--------|-------------|----------|
 | TRANSACTION | The email confirms, updates, or completes a transaction with the sender. | Order confirmation, subscription reciepts, website's terms of service updates, etc. |
 | SALE | The email is a sale or promotion from the sender to the recipient. This includes announcing a sale, a new product, a new service, etc. | A credit card company offering a new credit card, Amazon announcing a new product, etc. |
+| SCHOOL | The email is about school or daycare related activity. Goddard is a daycare center. | Goddard announcing a new policy, a school announcing a new event, daycare asking parents to bring in diapers, etc. |
+| REVIEW | The email is about a review of a product or service. | Amazon asking the customer to review their purchase, a hotel asking the customer to review their stay, etc. |
 | INQUIRY | The email is direct request for more information from the sender to the recipient. | A financial advisor asking for a document, a doctor asking for a signature, etc. |
 | ALERT | The email is a warning or alert. | A credit card company alerting the cardholder of a suspicious activity, Google alerting the user of a new sign in, etc. |
-| SOCIAL | The email is a social media notification. | A social media platform notifying the user of a new follower, a new post, etc. |
+| SOCIAL | The email is a social media notification. Social media sites include Patreon, Facebook, Instagram, Twitter, etc. | A social media platform notifying the user of a new follower, Patreon notifying of a new 'Play to Win' post, etc. |
 | OTHER | The email does not fall under any of the other categories. | A social media notification, a newsletter, a survey request, etc. |
 
 
