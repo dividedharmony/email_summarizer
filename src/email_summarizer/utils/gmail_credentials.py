fbebd774fbf144d3ba45da57dffd1cfad188dfff
David Harmon <dharmon202@gmail.com>
2025-04-26 18:41:51 -0400

    Fix credentials code

--- src/email_summarizer/utils/gmail_credentials.py
+++ src/email_summarizer/utils/gmail_credentials.py
@@ -4,13 +4,17 @@ from google.oauth2.credentials import Credentials
 
 from email_summarizer.models.enums import EmailAccounts
 
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]  # Read-only access
+
 
 def build_gmail_credentials(email_account: EmailAccounts) -> Credentials:
     return Credentials(
-        token=_get_env_var(email_account, "TOKEN"),
+        token=None,
         refresh_token=_get_env_var(email_account, "REFRESH_TOKEN"),
+        token_uri=os.getenv("GMAIL_TOKEN_URI"),  # same for all clients
         client_id=_get_env_var(email_account, "CLIENT_ID"),
         client_secret=_get_env_var(email_account, "CLIENT_SECRET"),
+        scopes=SCOPES,
     )
 
 
