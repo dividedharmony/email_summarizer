e0f41ba3b1d197096f46e3f5b2b1ab29b9a4126c
David Harmon <dharmon202@gmail.com>
2025-04-28 13:03:14 -0400

    Add high priority emails to controller

--- src/email_summarizer/controllers/alphonse_controller.py
+++ src/email_summarizer/controllers/alphonse_controller.py
@@ -41,6 +41,7 @@ async def put_email_report(
         print(f"Found channel: {channel.name} ({channel.id})")
 
         try:
+            # Get emails and compile report
             emails = get_emails(email_account, max_results=max_emails)
             grouping_payload = group_emails(emails)
             email_report = summarize_emails(
@@ -48,19 +49,37 @@ async def put_email_report(
                 emails=grouping_payload.get("ungrouped_emails", []),
                 grouped_emails=grouping_payload.get("list_of_grouped_emails", []),
             )
+
+            # Send report to channel
             await channel.send(_report_header(email_report))
             if email_report.is_empty():
-                await channel.send("No emails to report.")
+                await channel.send("*No emails to report.*")
             else:
+                # Display high priority emails
+                for i, high_priority in enumerate(email_report.high_priority_summaries):
+                    await channel.send(
+                        f"{i + 1}. ({high_priority.email.sender}) {high_priority.next_steps}"
+                    )
+                else:
+                    await channel.send("*No high priority emails to report.*")
+
+                # Display regular emails
                 for i, summary in enumerate(email_report.summaries):
                     await channel.send(
                         f"{i + 1}. ({summary.email.sender}) {summary.body}"
                     )
-                await channel.send("### Grouped Emails")
-                for grouped_email in email_report.grouped_emails:
-                    await channel.send(
-                        f"- ({grouped_email.sender}) - message count: {grouped_email.count}"
-                    )
+                else:
+                    await channel.send("*No regular emails to report.*")
+
+                # Display grouped emails
+                if _any_grouped_emails(email_report):
+                    await channel.send("### Grouped Emails")
+                    for grouped_email in email_report.grouped_emails:
+                        await channel.send(
+                            f"- ({grouped_email.sender}) - message count: {grouped_email.count}"
+                        )
+                else:
+                    await channel.send("*No grouped emails to report.*")
             print(f"Message sent to #{channel.name}")
         except EmailUnavailableError as e:
             print(f"Error: {e}")
@@ -78,3 +97,7 @@ async def put_email_report(
         # If you want the bot to stay online for other tasks, remove this line.
         print("Closing bot connection.")
         await client.close()
+
+
+def _any_grouped_emails(email_report: EmailReport) -> bool:
+    return any(grouped_email.count > 0 for grouped_email in email_report.grouped_emails)
